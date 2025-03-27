d81012e0cd132d29150c462c833000548888ec71
VasudevGoud <vasudev0951@gmail.com>
2025-03-27 11:15:39 +0100

    updated server code

--- server.py
+++ server.py
@@ -1,47 +1,56 @@
 import socket
 import threading
 
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = 'DISCONNECT !'
 
-HEADER=64   #bytes
-PORT=5050
-SERVER=socket.gethostbyname(socket.gethostname())
-ADDR=(SERVER,  PORT )
-FORMAT= 'utf-8'
-DISCONNECT_MESSAGE= 'DISCONNECT !'
-server =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def handle_client(conn, addr):
-    print(f"[new connection]{addr} connected")
+clients = []  # Store all connected clients
 
-    connected= True
+def broadcast(message, sender_conn):
+    """Send message to all clients except sender."""
+    for client in clients:
+        if client != sender_conn:
+            client.send(message)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    clients.append(conn)
+
+    connected = True
     while connected:
-      try:
-        msg_length=conn.recv(HEADER).decode()
-        msg_length=int(msg_length)
-        msg=conn.recv(msg_length).decode(FORMAT)
-        print(f"[{addr}] {msg}")
-        if msg==DISCONNECT_MESSAGE:
-            connected=False
-      except:
-        break
-        
+        try:
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+                
+                if msg == DISCONNECT_MESSAGE:
+                    connected = False
+                else:
+                    print(f"[{addr}] {msg}")
+                    broadcast(msg.encode(FORMAT), conn)
+        except:
+            break
+
+    clients.remove(conn)
     conn.close()
     print(f"[DISCONNECTED] {addr} disconnected.")
-        
-
-    
 
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr=server.accept()
-        thread=threading.Thread(target=handle_client, args=(conn,addr))
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
-
-print("[STARTING] server is  starting...")
+print("[STARTING] Server is starting...")
 start()
